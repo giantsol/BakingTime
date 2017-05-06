9955feb55a41a45cf67485cb274c28a254fa22fe
Hansol Lee <giantsol@naver.com>
2017-05-06 22:46:45 +0900

    Add a little magnetic animation when recipe item is clicked.

--- app/src/main/java/com/lee/hansol/bakingtime/adapters/RecipesRecyclerViewAdapter.java
+++ app/src/main/java/com/lee/hansol/bakingtime/adapters/RecipesRecyclerViewAdapter.java
@@ -1,11 +1,19 @@
 package com.lee.hansol.bakingtime.adapters;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +24,9 @@ import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.R.attr.id;
+import static android.R.attr.translationZ;
 
 public class RecipesRecyclerViewAdapter extends RecyclerView.Adapter<RecipesRecyclerViewAdapter.RecipeViewHolder> {
     private Context context;
@@ -61,6 +72,7 @@ public class RecipesRecyclerViewAdapter extends RecyclerView.Adapter<RecipesRecy
         @BindView(R.id.recipe_list_item_name) TextView name;
         @BindView(R.id.recipe_list_item_servings) TextView servings;
         @BindView(R.id.recipe_list_item_image) ImageView image;
+        @BindView(R.id.recipe_list_item_parent) CardView parent;
 
         RecipeViewHolder(View view) {
             super(view);
@@ -70,7 +82,30 @@ public class RecipesRecyclerViewAdapter extends RecyclerView.Adapter<RecipesRecy
 
         @Override
         public void onClick(View v) {
-            recipeItemClickListener.onRecipeItemClick(recipes[getAdapterPosition()]);
+            Animator animator = AnimatorInflater.loadAnimator(context, R.animator.raise);
+            animator.setTarget(v);
+            animator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    recipeItemClickListener.onRecipeItemClick(recipes[getAdapterPosition()]);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            animator.start();
         }
     }
 }
