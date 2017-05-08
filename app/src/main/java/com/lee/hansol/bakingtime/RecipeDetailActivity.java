9f8588bdb64e917b4f4cdb6ad7efd409e5773943
Hansol Lee <giantsol@naver.com>
2017-05-08 18:26:59 +0900

    Refactor drawer related codes

--- app/src/main/java/com/lee/hansol/bakingtime/RecipeDetailActivity.java
+++ app/src/main/java/com/lee/hansol/bakingtime/RecipeDetailActivity.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.lee.hansol.bakingtime.adapters.RecipesRecyclerViewAdapter;
 import com.lee.hansol.bakingtime.adapters.StepsRecyclerViewAdapter;
 import com.lee.hansol.bakingtime.fragments.RecipeStepDetailFragment;
 import com.lee.hansol.bakingtime.fragments.RecipeStepListFragment;
@@ -21,17 +22,16 @@ import com.lee.hansol.bakingtime.models.Step;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static android.R.id.toggle;
+import static com.lee.hansol.bakingtime.utils.LogUtils.log;
 import static com.lee.hansol.bakingtime.utils.ToastUtils.toast;
 
 public class RecipeDetailActivity extends AppCompatActivity
         implements StepsRecyclerViewAdapter.OnStepItemClickListener {
     private Recipe recipe;
+    private ActionBarDrawerToggle drawerToggle;
 
-    @BindView(R.id.activity_recipe_detail_navigation_drawer) DrawerLayout navigationDrawer;
-    @BindView(R.id.activity_recipe_detail_navigation_drawer_view) ListView navigationDrawerView;
-
-    private ActionBarDrawerToggle toggle;
+    @BindView(R.id.activity_recipe_detail_navigation_drawer) DrawerLayout drawer;
+    @BindView(R.id.activity_recipe_detail_navigation_drawer_view) ListView drawerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,19 +46,11 @@ public class RecipeDetailActivity extends AppCompatActivity
 
     private void initialize() {
         recipe = getIntent().getParcelableExtra(MainActivity.INTENT_EXTRA_RECIPE_OBJECT);
+        initializeDrawer();
+    }
 
-        navigationDrawerView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
-                new String[]{"hello", "there"}));
-        navigationDrawerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                toast(RecipeDetailActivity.this, (String) parent.getItemAtPosition(position));
-            }
-        });
-
-        toggle = new ActionBarDrawerToggle(this, navigationDrawer,
-                R.string.content_description_drawer_open,
-                R.string.content_description_drawer_close) {
+    private void initializeDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.content_description_drawer_open, R.string.content_description_drawer_close) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -68,40 +60,31 @@ public class RecipeDetailActivity extends AppCompatActivity
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle("Choose recipe");
+                getSupportActionBar().setTitle(R.string.text_choose_recipe);
             }
         };
-        navigationDrawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(drawerToggle);
+        initializeDrawerView();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void initializeDrawerView() {
+        drawerView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                new String[]{"hello", "there"}));
+        drawerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                toast(RecipeDetailActivity.this, (String) parent.getItemAtPosition(position));
+            }
+        });
     }
 
     private void addFragments() {
         boolean isTablet = findViewById(R.id.activity_recipe_detail_step_list_fragment_container) != null;
         if (isTablet) setTabletLayout();
         else setNonTabletLayout();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        toggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        toggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (toggle.onOptionsItemSelected(item)) {
-          return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void setTabletLayout() {
@@ -118,6 +101,23 @@ public class RecipeDetailActivity extends AppCompatActivity
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.activity_recipe_detail_fragment_container, stepListFragment)
                 .commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     @Override
